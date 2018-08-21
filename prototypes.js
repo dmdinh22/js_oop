71e8404d68db7b33f73e42aa0fa410af32267ffc
David Dinh <dmdinh22@gmail.com>
2018-08-21 00:06:35 -0700

    prototypes

---
+++ prototypes.js
@@ -0,0 +1,31 @@
+// constructor
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
+
+// getSummary on prototype
+Book.prototype.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}.`;
+};
+
+// getAge
+Book.prototype.getAge = function() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old.`;
+};
+
+// revise / change year - making changes on the obj from prototype method
+Book.prototype.revise = function(newYear) {
+    this.year = newYear;
+    this.revised = true;
+};
+
+// instantiate an object
+const book1 = new Book('Book One', 'John Doe', '2013');
+const book2 = new Book('Book Two', 'Jane Doe', '2016');
+
+console.log(book2);
+book2.revise('2018');
+console.log(book2);
